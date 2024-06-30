4ea39546f3eb12133cef9d166189f94aac89a267
zioms0n777 <151380545+zioms0n777@users.noreply.github.com>
2024-06-30 17:58:22 +0200

    Initial commit

---
+++ src/game/Main.java
@@ -0,0 +1,12 @@
+package game;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		TicTacToe tictactoe = new TicTacToe();
+		
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module TicTacToe {
+	requires java.desktop;
+}
